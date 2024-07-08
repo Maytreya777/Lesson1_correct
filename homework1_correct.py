c56698466aee704eda80f4a81770b1fb20836e7c
Maytreya777 <maytreya777@gmail.com>
2024-07-08 12:45:14 +0300

    Add files via upload

---
+++ homework1_correct.py
@@ -0,0 +1,8 @@
+example=("Чебурашка")
+print(example[0])# Ч
+print(example[-1::]) #а
+print(len(example))# 9
+print(example[4:]) # рашка
+print(example[::-1]) #  акшарубеЧ
+print(example[1::2]) # еуак
+
